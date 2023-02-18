22db9372e2d330e640fdc5293e6a82276b2b9a3d
aaronrubinstein <40306940+aaronrubinstein@users.noreply.github.com>
2023-02-18 06:42:23 +0000

    updates

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<div class="container">
+    <header>
+       <span class="title">Todo</span>
+       <img src="images/icon-moon.svg" alt="Dark theme toggle">
+    </header>
+</div>
+
+<style>
+
+    .container {
+        max-width: 540px;
+        margin: 70px auto 50px auto;
+    }
+
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .title {
+        font-size: 40px;
+        font-weight: 700;
+        letter-spacing: 15px;
+        text-transform: uppercase;
+        color: #FFF;
+    }
+
+</style>
